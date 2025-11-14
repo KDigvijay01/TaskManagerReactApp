74fdbb10a9dadde1feb788c90e5f926b59c9387f
dkumar-kreeda <digvijay.kumar@kreedalabs.com>
2025-11-14 18:31:36 +0530

    added backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://task-manager-react-app-htud.vercel.app/api',
   timeout: 10000,
 });
 
